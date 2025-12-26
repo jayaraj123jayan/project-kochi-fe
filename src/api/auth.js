1a470afff6fb9b98adb2bcdbe3245dc35b93086c
jayaraj <jayaraj123jayan@gamil.com>
2025-12-26 22:20:58 +0530

    hostchanged

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,7 +3,7 @@ import { API_BASE_URL } from '../constants/api';
 
 export const loginUser = async (email, password) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}/login`, { email, password });
+    const response = await axios.post(`/login`, { email, password });
     return response.data; // Assume it returns user object
   } catch (error) {
     throw error.response ? error.response.data : error.message;
